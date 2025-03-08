ca1b2ea8d31753e2027ec35643b1dedd70ffcd0a
Jordan Hamilton <jordanh6489@hotmail.com>
2025-03-07 21:37:18 -0500

    Gui and background updated

--- Encryption/Encryption/Form1.Designer.cs
+++ Encryption/Encryption/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderSelectBtn = new System.Windows.Forms.Button();
+            this.selectedFileLabel = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(131, 33);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(156, 16);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Good Enough Encryption";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select the file";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // folderSelectBtn
+            // 
+            this.folderSelectBtn.Location = new System.Drawing.Point(178, 119);
+            this.folderSelectBtn.Name = "folderSelectBtn";
+            this.folderSelectBtn.Size = new System.Drawing.Size(75, 23);
+            this.folderSelectBtn.TabIndex = 2;
+            this.folderSelectBtn.Text = "Select";
+            this.folderSelectBtn.UseVisualStyleBackColor = true;
+            this.folderSelectBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // selectedFileLabel
+            // 
+            this.selectedFileLabel.AutoSize = true;
+            this.selectedFileLabel.Location = new System.Drawing.Point(12, 157);
+            this.selectedFileLabel.Name = "selectedFileLabel";
+            this.selectedFileLabel.Size = new System.Drawing.Size(44, 16);
+            this.selectedFileLabel.TabIndex = 3;
+            this.selectedFileLabel.Text = "label2";
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(161, 241);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(100, 22);
+            this.passwordBox.TabIndex = 4;
+            this.passwordBox.Enter += new System.EventHandler(this.passwordBox_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please enter the password";
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(99, 296);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.encryptBtn.TabIndex = 6;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(203, 295);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.decryptBtn.TabIndex = 7;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(430, 629);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.selectedFileLabel);
+            this.Controls.Add(this.folderSelectBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "GEES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button folderSelectBtn;
+        private System.Windows.Forms.Label selectedFileLabel;
+        private System.Windows.Forms.MaskedTextBox passwordBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button decryptBtn;
     }
 }
 
--- Encryption/Encryption/Form1.cs
+++ Encryption/Encryption/Form1.cs
@@ -10,11 +10,56 @@ using System.Windows.Forms;
 
 namespace Encryption
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //sets all the background stuff up
+            Form form = (Form)sender;
+
+            form.BackColor = Color.DarkGray;
+            form.StartPosition = FormStartPosition.CenterScreen;
+
+            selectedFileLabel.Visible = false;
+
+            encryptBtn.Enabled = false;
+            decryptBtn.Enabled = false;
+            passwordBox.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //uses the button to open a dialog box then show the selected file
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+
+            if (dlg.FileName != null && dlg.FileName != "")
+            {
+                selectedFileLabel.Text = "Selected File: " + dlg.FileName;
+                selectedFileLabel.Visible = true;
+
+                passwordBox.Enabled = true;
+            }
+        }
+
+        private void passwordBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
