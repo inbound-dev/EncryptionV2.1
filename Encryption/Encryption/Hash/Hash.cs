071a0180196ef13eeb8b98bc0836ce785eaf02f2
Jordan Hamilton <jordanh6489@hotmail.com>
2025-03-11 07:35:46 -0400

    quite a few additions

--- Encryption/Encryption/Hash/Hash.cs
+++ Encryption/Encryption/Hash/Hash.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Encryption.HashFunction
 {
@@ -12,21 +13,52 @@ namespace Encryption.HashFunction
 
         public String NewHash(string inputKey)
         {
-            String finalKey = "";
+            string proccessedString = "";
+            string hashedString;
 
-            //extend length to 32 chars
-            if (inputKey.Length < 32)
+            proccessedString = RepeatOrTrimString(inputKey);
+            hashedString = HashGivenString(proccessedString);
+
+            return hashedString;
+        }
+
+        public static string HashGivenString(string input)
+        {
+            //turns the given string into its ASCII representation
+            string numericalRep = "";
+            foreach (char key in input)
             {
-                Console.WriteLine("hey");
-                for (int i = 0; i < (32 / inputKey.Length); i++)
-                {
-                    inputKey.Insert(inputKey.Length, "q");
-                }
-                finalKey = inputKey;//.Length.ToString();
+                numericalRep += (int)key;
             }
 
+            //turns given string into its binary representation
+            string binaryRep = "";
+            foreach (char val in numericalRep)
+            {
+                binaryRep += AsciiToBinary(val);
+            }
 
-            return finalKey;
+            return binaryRep;
         }
+
+        //converts characters from ASCII to binary
+        public static string AsciiToBinary(int ascii)
+        {
+            return Convert.ToString(ascii, 2).PadLeft(8, '0'); // Converts to binary with 8-bit padding
+        }
+
+        //takes the oringnal string and makes it 32 characters long
+        public static string RepeatOrTrimString(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return new string(' ', 32); // Handle empty input
+
+            while (input.Length < 32)
+            {
+                input += input; // Repeat the string
+            }
+
+            return input.Substring(0, 32); // Trim to 32 characters
+        }
+
     }
 }
